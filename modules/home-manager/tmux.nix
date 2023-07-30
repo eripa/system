b44e2cf8141f03f14bdfacd250c255a798ccf78c
Eric Ripa <eric@ripa.io>
2023-07-30 16:14:57 -0700

    feat: adds tmux config

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -1,5 +1,34 @@
 {...}: {
   programs.tmux = {
     enable = true;
+    aggressiveResize = true;
+    historyLimit = 100000;
+    mouse = false;
+    terminal = "xterm-256color";
+    clock24 = true;
+    extraConfig = ''
+      ## general
+      ${builtins.readFile ./tmux-shared.conf}
+
+      # make it easy to send C-b
+      bind-key b send-prefix
+
+      # prefix again goes to last window
+      bind-key C-b last-window
+
+      # reload
+      bind r source-file ~/.tmux.conf
+
+      # keybindings to make resizing easier
+      bind -r C-h resize-pane -L
+      bind -r C-j resize-pane -D
+      bind -r C-k resize-pane -U
+      bind -r C-l resize-pane -R
+
+      # make it so that I can hold down prefix key for these
+      bind-key C-d detach
+      bind-key C-n next-window
+      bind-key C-p previous-window
+    '';
   };
 }
